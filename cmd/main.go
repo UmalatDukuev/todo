75af8e05379ea1db30740496cb68c08a4154fae5
Umalat <dukuev037@mail.ru>
2024-10-05 19:34:36 +0300

    бд

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,26 @@ import (
 	"todo/pkg/handler"
 	"todo/pkg/repository"
 	"todo/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initialising configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
-
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }

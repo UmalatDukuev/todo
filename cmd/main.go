c3fc9b1b9a0edd3f63f3976ec6c7ff8b4301fbf3
Umalat <dukuev037@mail.ru>
2024-12-04 23:17:30 +0300

    1

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package cmd
+
+import "log"
+
+func main() {
+
+	srv := new(todo.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			log.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+}

d221276fe06d737781b30acefbe032af8164becf
Umalat <dukuev037@mail.ru>
2024-12-23 00:42:17 +0300

    todo

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,51 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/UmalatDukuev/todo"
 	"github.com/UmalatDukuev/todo/pkg/handler"
+	"github.com/UmalatDukuev/todo/pkg/repository"
+	"github.com/UmalatDukuev/todo/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 	// if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,16 +1,20 @@
 package handler
 
 import (
+	"github.com/UmalatDukuev/todo/pkg/service"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
-	//services *service.Service
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
-
 
 	auth := router.Group("/auth")
 	{
